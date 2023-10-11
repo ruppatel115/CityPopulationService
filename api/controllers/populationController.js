5b845da8ca79b608dfcd8d7d493fe086d01947f5
thegoat <ruppatel11599@gmail.com>
2023-10-11 19:38:03 -0400

    Updates to save function

--- api/controllers/populationController.js
+++ api/controllers/populationController.js
@@ -5,7 +5,7 @@ let populationDataMap = new Map();
 
 function loadPopulationData() {
   return new Promise((resolve, reject) => {
-    fs.createReadStream('../api/data/city_populations.csv')
+    fs.createReadStream('./api/data/city_populations.csv')
       .pipe(csv())
       .on('data', (row) => {
         const city = row.city.trim();
@@ -22,12 +22,16 @@ function loadPopulationData() {
   });
 }
 
-async function savePopulationData() {
-  const dataLines = Array.from(populationDataMap).map(([key, value]) => {
+async function savePopulationData(dataMap) {
+  const dataLines = Array.from(dataMap).map(([key, value]) => {
     const [city, state] = key.split(',');
     return `${city},${state},${value}`;
   });
-  await fs.promises.writeFile('../api/data/city_populations.csv', dataLines.join('\n'));
+
+  await fs.promises.writeFile('src/data/population.csv', dataLines.join('\n'));
+
+  // Update the in-memory map with the new data
+  populationDataMap = dataMap;
 }
 
 module.exports = {
